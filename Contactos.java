e9b9c2649b7b3abd74eb7e686e81df989e156cfd
RusveltMeza <101429716+RusveltMeza@users.noreply.github.com>
2023-11-29 01:03:54 -0500

    Create Contactos.java

---
+++ Contactos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad6;
+
+/**
+ *
+ * @author rusve
+ */
+public class Actividad6 {
+
+    public static void main(String[] args) {
+        VentanaContactos ventana = new VentanaContactos();
+        ventana.setVisible(true);
+    }
+    
+}
